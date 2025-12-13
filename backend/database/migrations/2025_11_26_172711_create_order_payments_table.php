7a0fe13df2ecc544e77fd93f3ae509337ecd982a
Zachariah Ndungu <ndunguzack@gmail.com>
2025-12-13 21:30:29 +0300

    created database structures for orders. run database migrations.

---
+++ backend/database/migrations/2025_11_26_172711_create_order_payments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+
+            $table->enum('status', [
+                'pending',
+                'paid',
+                'failed',
+                'refunded'
+            ])->default('pending');
+
+            $table->string('payment_method')->nullable();
+            $table->string('transaction_id')->nullable();
+
+            $table->decimal('amount', 10, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_payments');
+    }
+};
